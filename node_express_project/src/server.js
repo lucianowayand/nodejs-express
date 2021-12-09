9c6e09cede33547ed01a4b8e219a192ea87a23b4
Luciano <lucianowayand@hotmail.com>
2021-12-09 00:18:57 -0300

    idk

--- node_express_project/src/server.js
+++ node_express_project/src/server.js
@@ -5,11 +5,11 @@ const dir = __dirname
 
 //Routes
 app.get("/", function(req, res){
-    res.sendFile(dir+"/views/main/index.html")
+    res.send("Home")
 })
 
 app.get("/about_us", function(req, res){
-    res.sendFile(dir+"/views/about/index.html")
+    res.send("About us")
 })
 
 
